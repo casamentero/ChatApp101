dc2456092e7f975aa712e5e32f5402c4cb2a703f
Mahabir <mahavir9008@gmail.com>
2016-05-19 00:35:45 +0530

    Added RabbitMQ Lib.
    Added Consumer Code to Retrieve The Message.

--- app/src/main/java/source/app/chat/chatapp/MainActivity.java
+++ app/src/main/java/source/app/chat/chatapp/MainActivity.java
@@ -1,31 +1,45 @@
 package source.app.chat.chatapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.UnsupportedEncodingException;
 
+import source.app.chat.chatapp.Utility.Constant;
+import source.app.chat.chatapp.consumer.MessageConsumer;
+
+public class MainActivity extends Activity {
+
+    private MessageConsumer mConsumer;
+    private TextView mOutput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+// The output TextView we'll use to display messages
+        mOutput = (TextView) findViewById(R.id.output);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        // Create the consumer
+        mConsumer = new MessageConsumer(Constant.Server.IP_ADDRESS, Constant.Server.EXCHANGE_NAME, Constant.Server.EXCHANGE);
+
+        // register for messages
+        mConsumer.setOnReceiveMessageHandler(new MessageConsumer.OnReceiveMessageHandler() {
+
+            public void onReceiveMessage(byte[] message) {
+                String text = "";
+                try {
+                    text = new String(message, "UTF8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                mOutput.append("\n" + text);
             }
         });
+
     }
 
     @Override
---
+++ app/src/main/java/source/app/chat/chatapp/Utility/Constant.java
@@ -0,0 +1,18 @@
+package source.app.chat.chatapp.Utility;
+
+public class Constant {
+
+    public static class Server {
+        public static final String IP_ADDRESS = "162.144.209.190";
+      //  public static final String IP_ADDRESS = "10.0.2.2";
+        public static final String QUEUE_NAME = "message.user.25";
+    	public static final String EXCHANGE_NAME = "message_ex";
+    	public static final String EXCHANGE = "topic";
+//        5672
+    }
+
+    public static class Credentials {
+        public static final String USERNAME = "test";
+        public static final String PASSWORD = "test";
+    }
+}
